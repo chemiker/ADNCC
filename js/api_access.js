5d4422b9f085fb7277e1a7486cd87529ff33d116
sylar-5 <sylar@sylar-5.de>
2013-05-15 21:51:31 +0200

    seperated config from api_access

--- js/api_access.js
+++ js/api_access.js
@@ -1,26 +1,3 @@
-//config for api calls
-api_config = {
-    /*
-     * if you are calling from a different domain, you need to set up a proxy.
-	 * this means, you need a url on your own server, which redirects the query (the part of the url behind the "?") to the actual api
-	 *
-	 * this is an example php file (one line):
-	 *    <?php echo file_get_contents("http://adn-client-comparison.nigma.de/pull.php?".$_SERVER['QUERY_STRING']); ?>
-	 *
-	 * The url can be absolute or relative to the executing html file, but should not conain the domain name (see XMLHttpRequest)
-	 *
-	 */
-	url: "api-1.1/array.php",
-	
-	/*
-	 * This should contain accepted api params
-	 */
-	accepted_params: ["name","filter","show","hide","format"]
-
-}
-
-
-
 /*
  * arguments:
  *  params: {key:value,key:value} to pass as ?key=value&key=value}
@@ -82,5 +59,4 @@ function api_columns() {
 	api_config.header_polled = query_api({format:"header_inverse"});
 	return api_config.header_polled;	
 }
-
 
